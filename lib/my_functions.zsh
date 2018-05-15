11fe249f4f4088edb106d2d0562722aaa0e1a9b6
blaedj <blaedj@gmail.com>
2018-05-15 09:31:09 -0500

    add some kubectl shortcut functions

--- lib/my_functions.zsh
+++ lib/my_functions.zsh
@@ -17,3 +17,26 @@ function explain {
   # opens url in browser
   open $url
 }
+
+# thanks to github.com/NickTitle for the following
+#
+# parses out the lcocal IP address as LOCALHOST, then runs docker-compose up
+dcu() {
+    LOCALHOST=`ipconfig getifaddr en0` docker-compose up
+}
+
+# kpod pr-868
+kpod() {
+    kubectl get pods -l app="cloud-$1" -o name
+}
+
+# kexec pr-867 cloud /bin/bash
+kexec() {
+    kubectl exec -it `kpod $1` -c $2 $3
+}
+
+# klog pr-867 cloud
+klog() {
+    kubectl logs -f `kpod $1` -c $2
+}
+
