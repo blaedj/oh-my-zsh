2a3cb37c87d7342a4bc3094b24c3c15c48c8ad14
blaedj <blaedj@gmail.com>
2020-04-07 07:14:55 -0500

    update klaunch utility

--- lib/launcher.zsh
+++ lib/launcher.zsh
@@ -9,7 +9,7 @@
 # ./build/cloudctl create enroll-secret --tenant dababi >  ~/tmp/local_secret
 # for local.
 
-klaunch() {
+k1launch() {
     case $1 in
         "master" )
             ~/code/go/src/github.com/kolide/launcher/build/launcher \
@@ -29,8 +29,62 @@ klaunch() {
                 -hostname localhost:8800 \
                 -enroll_secret_path ~/tmp/local_secret \
                 -debug \
-                --insecure \
-                --insecure_grpc \
+                -insecure_transport \
+                -insecure \
                 2>&1 | tee ~/tmp/local.log ;;
+    esac
+}
+
+klaunch() {
+    case $1 in
+        "help")
+            echo "please provide the operating system of the launcher host: {mac,centos,ubuntu,macold}" ;;
+        "mac" )
+            ~/code/go/src/github.com/kolide/launcher/build/launcher \
+                -root_directory $(mktemp -d) \
+                -hostname localhost:5000 \
+                -enroll_secret_path ~/code/rails/k2/tmp/secret.txt \
+                --insecure \
+                --insecure_transport \
+                --osqueryd_path "/usr/local/kolide-k2/bin/osqueryd" \
+                -transport jsonrpc \
+                -debug \
+                -with_initial_runner \
+                -disable_control_tls \
+                2>&1 | tee ~/tmp/local.log ;;
+        "macold" )
+            ~/tmp/oldlauncher \
+                -root_directory $(mktemp -d) \
+                -hostname localhost:5000 \
+                -enroll_secret_path ~/code/rails/k2/tmp/secret.txt \
+                --insecure \
+                --insecure_transport \
+                -transport jsonrpc \
+                --debug true \
+                -with_initial_runner \
+                -disable_control_tls \
+                2>&1 | tee ~/tmp/local.log ;;
+        "ubuntu" )
+            docker run -i -t gcr.io/kolide-public-containers/launcher-fakedata-ubuntu18:latest \
+                   -debug \
+                   -hostname host.docker.internal:5000 \
+                   -control \
+                   -control_hostname host.docker.internal:5000 \
+                   -transport jsonrpc \
+                   -enroll_secret $(cat ~/code/rails/k2/tmp/secret.txt) \
+                   -insecure_transport \
+                   -insecure \
+                   -disable_control_tls ;;
+        "centos" )
+            docker run -i -t gcr.io/kolide-public-containers/launcher-fakedata-centos7:latest \
+                   -debug \
+                   -hostname host.docker.internal:5000 \
+                   -control \
+                   -control_hostname host.docker.internal:5000 \
+                   -transport jsonrpc \
+                   -enroll_secret $(cat ~/code/rails/k2/tmp/secret.txt) \
+                   -insecure_transport \
+                   -insecure \
+                   -disable_control_tls ;;
     esac
 }
